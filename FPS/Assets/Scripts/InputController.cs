f08b0c562537b16a8fe7d126268c45e718f27261
Roman <74704965+KengiTakery0@users.noreply.github.com>
2023-09-10 10:01:49 +0300

    Add weapons Setup test scene/edit animations

---
+++ FPS/Assets/Scripts/InputController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputController : MonoBehaviour
+{
+    PlayerInput input;
+    PlayerInput.OnFootActions onFootActions;
+
+    Player _player;
+
+    private void Awake()
+    { 
+        input = new PlayerInput();
+        onFootActions = input.OnFoot;
+        _player = GetComponent<Player>();
+    }
+
+
+    private void FixedUpdate()
+    {
+        _player.Move(onFootActions.Move.ReadValue<Vector2>());
+    }
+
+
+    #region ENABLE/Diable
+    private void OnEnable()
+    {
+        input.Enable();
+    }
+
+    private void OnDisable()
+    {
+        input.Disable();
+    }
+    #endregion
+}
